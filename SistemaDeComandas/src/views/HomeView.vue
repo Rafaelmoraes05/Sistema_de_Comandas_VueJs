bd59128e9706a490d744d6cd591338b269cd666b
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-05-16 14:47:42 -0300

    [refactor]: Alteração do router index.js, HomeView, AboutView e App.vue
    Index.js: alterado a rota do About para o nome componente;
    HomeView: alterado todo o componente, adicionado o Banner;
    AboutView: alterado para PedidosView para representar a tela de pedidos;
    App.vue: adicionado o Navbar e o Footer como componentes permanentes da aplicação

--- SistemaDeComandas/src/views/HomeView.vue
+++ SistemaDeComandas/src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import Banner from '../components/Banner.vue'
+import BurgerForm from '../components/BurgerForm.vue'
+
+export default{
+  components:{
+    Banner,
+    BurgerForm
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="main-container">
+    <Banner />
+    <h1>Monte o seu Burger</h1>
+    <BurgerForm />
+  </div>
 </template>
+<style>
+h1{
+  font-weight: 800;
+  margin-top: 30px;
+}
+</style>
