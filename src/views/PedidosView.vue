a606b35ca0d3b817dde884dc4184c16579f7378b
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-05-17 17:28:54 -0300

    [refactor]: Refatorando o PedidoView para exibir o Componente Dashboard

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
         <h1>Pedidos</h1>
+        <Dashboard />
     </div>
 </template>
+
+<script>
+    import Dashboard from '../components/Dashboard.vue';
+
+    export default {
+        name:'PedidosView',
+        components:{
+            Dashboard
+        }
+    }
+</script>
